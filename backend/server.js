263ca80e6d2510f7050120e21302ed3dc1250295
manivardhan9999 <140253994+manivardhan9999@users.noreply.github.com>
2025-07-21 18:28:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,15 @@ app.use(bodyParser.json());
 // Routes
 app.use('/api/schemas', schemaRoutes);
 
-// MongoDB Connection
-mongoose.connect('mongodb+srv://manivardhan9999:REut9ZEAyPQvk8wv@cluster0.yrfloda.mongodb.net/schema-builder?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
+// MongoDB Connection
+// mongoose.connect('mongodb+srv://manivardhan9999:REut9ZEAyPQvk8wv@cluster0.yrfloda.mongodb.net/schema-builder?retryWrites=true&w=majority', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
 
 .then(() => {
   console.log('Connected to MongoDB');
